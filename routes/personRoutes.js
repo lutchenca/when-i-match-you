1809a66e5ab540c453660c0ff71e5df9e6c55cad
Lut <lutchenca@hotmail.com>
2022-02-08 18:07:57 -0300

    create delete method && update gitignore

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -70,4 +70,20 @@ router.patch("/:id", async (req, res) => {
   }
 });
 
+router.delete('/:id',  async (req, res) => {
+    const id = req.params.id 
+    const person = await Person.findOne({ _id: id });
+    if (!person) {
+      res.status(422).json({ message: "usuario não encontrado" });
+      return;
+    }
+
+    try {
+        await Person.deleteOne({_id:id})
+        res.status(200).json({message: "usuário deletado com sucesso"})
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+});
+
 module.exports = router;
