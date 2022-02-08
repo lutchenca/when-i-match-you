d29f2f2f446e3d177407a8ad0d86847f4e51adef
Lut <lutchenca@hotmail.com>
2022-02-08 17:51:27 -0300

    create routes and models

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 
-const Person = require("./models/Person")
 
 app.use(
   express.urlencoded({
@@ -12,6 +11,10 @@ app.use(
 );
 
 app.use(express.json());
+
+const personRoutes = require('./routes/personRoutes')
+
+app.use('/person', personRoutes)
 
 app.get("/", (req, res) => {
   res.json({ message: "oi!" });
@@ -22,12 +25,10 @@ const DB_PASSWORD = encodeURIComponent("w4bfNwaE6F2Fsz");
 
 mongoose
   .connect(
-    `mongodb+srv://${DB_USER}:<${DB_PASSWORD}>@cluster0.sk4tw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.sk4tw.mongodb.net/?retryWrites=true&w=majority`
   )
   .then(() => {
     console.log("mongoDB connection successfully!");
     app.listen(3000);
   })
   .catch((err) => console.log(err));
-
-app.listen(3000);
